e6e557c68b1cbc95189e377e9976ae6a4b79416f
nikhilsingla255 <singla255cool@gmail.com>
2026-02-02 12:19:33 +0530

    checking git push and pushing order controller

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 
-module.exports = async () => {
-    await mongoose.connect(process.env.MONGODB_URL)
-    console.log("database connected")
+// module.exports = async () => {
+//     await mongoose.connect(process.env.MONGODB_URL)
+//     console.log("database connected")
+// }
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("connected to database");
+    } catch (error) {
+        console.log("unable to connect to dbatabase");
+        console.log("error", error)
+        process.exit(1);
+    }
 }
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URL)
-//     } catch (error) {
-//         console.log("error", error)
-//         process.exit();
-//     }
-// }
+module.exports = connectDB
--- server/src/controllers/orderController.js
+++ server/src/controllers/orderController.js
@@ -5,25 +5,39 @@ const Product = require("../models/Product");
 //write CRUD operations
 exports.createOrder = async (req,res)=>{
     //creating an object to push some request body
+    try{
     const {productId,quantity,customerId} = req.body;
+    //generate the next order id
+    const lastOrder = (await Order.find()).toSorted({_id:-1})
+    .limit(1).select("_id");
+    const nextId = lastOrder[0]._id ? lastOrder[0]._id +1 : 1;
 
+    //fetch the product
     const product =   await Product.findById(productId);
     if(product.quantity <quantity){
         return res.status(400).json({message:"insufficeint stock"});
     }
 
-    const total =product.price * quantity;
+    const totalAmount =product.price * quantity; 
  
+    //create order
     const order = await Order.create({
         productId,
         quantity,
         price: product.price,
-        total,
-        customerId
+        totalAmount,
+        customerId,
+        orderDate: new Date()
     });
+
+    //reduce stock
     product.quantity -= quantity;
     await product.save();
+
     res.status(201).json(order);
+}catch(e){
+    console.log("error",e)
+}
 }
 
 exports.getOrders = async (req,res) =>{
